98486ce792bf9394d7f81ad4e8bdef7aa767a518
Mobile Develper <>
2018-01-31 10:34:45 +0100

    firebase updated fixed some issues

---
+++ app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+ 	  apiKey: "AIzaSyAkfeVgnI3uA_PFxQqqKiTITStF9cPdYMY",
+      authDomain: "react-native-chat-bcd0c.firebaseapp.com",
+      databaseURL: "https://react-native-chat-bcd0c.firebaseio.com",
+      projectId: "react-native-chat-bcd0c",
+      storageBucket: "react-native-chat-bcd0c.appspot.com",
+      messagingSenderId: "832583504245"
+}
